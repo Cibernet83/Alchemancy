6f8e5100ff4da45276434fda5e8b173ac53723f7
Cibernet <41699236+Cibernet83@users.noreply.github.com>
2024-10-17 14:49:29 -0400

    test

--- src/main/java/net/cibernet/alchemancy/properties/soulbind/VengefulProperty.java
+++ src/main/java/net/cibernet/alchemancy/properties/soulbind/VengefulProperty.java
@@ -2,11 +2,18 @@ package net.cibernet.alchemancy.properties.soulbind;
 
 import net.cibernet.alchemancy.properties.Property;
 import net.cibernet.alchemancy.util.ClientUtil;
+import net.minecraft.core.Holder;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
+import net.neoforged.neoforge.registries.DeferredItem;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class VengefulProperty extends Property
 {
@@ -20,6 +27,11 @@ public class VengefulProperty extends Property
 				ClientUtil.createTrackedParticles(event.getEntity(), ParticleTypes.ENCHANTED_HIT);
 		}
 
+	}
+
+	@Override
+	public Collection<ItemStack> populateCreativeTab(DeferredItem<Item> capsuleItem, Holder<Property> holder) {
+		return List.of();
 	}
 
 	@Override
