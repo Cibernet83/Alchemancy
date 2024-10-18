ee23132dd299a6ba9da704b38735803aa0fc1595
Cibernet <41699236+Cibernet83@users.noreply.github.com>
2024-10-17 21:51:52 -0400

    Extended now doubles Firework Flight Duration

--- src/main/java/net/cibernet/alchemancy/properties/ExtendedProperty.java
+++ src/main/java/net/cibernet/alchemancy/properties/ExtendedProperty.java
@@ -1,12 +1,15 @@
 package net.cibernet.alchemancy.properties;
 
 import net.cibernet.alchemancy.Alchemancy;
+import net.minecraft.core.component.DataComponentType;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.EquipmentSlotGroup;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.component.Fireworks;
 import net.neoforged.neoforge.event.ItemAttributeModifierEvent;
 
 public class ExtendedProperty extends Property
@@ -21,6 +24,14 @@ public class ExtendedProperty extends Property
 			event.addModifier(Attributes.STEP_HEIGHT, STEP_HEIGHT_MOD, EquipmentSlotGroup.FEET);
 		event.addModifier(Attributes.BLOCK_INTERACTION_RANGE, REACH_MOD, EquipmentSlotGroup.MAINHAND);
 		event.addModifier(Attributes.ENTITY_INTERACTION_RANGE, REACH_MOD, EquipmentSlotGroup.MAINHAND);
+	}
+
+	@Override
+	public <T> Object modifyDataComponent(ItemStack stack, DataComponentType<? extends T> dataType, T data)
+	{
+		if(dataType == DataComponents.FIREWORKS && data instanceof Fireworks fireworks)
+			return new Fireworks(fireworks.flightDuration(), fireworks.explosions());
+		return super.modifyDataComponent(stack, dataType, data);
 	}
 
 	@Override
