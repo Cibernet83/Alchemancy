849b6d92fc0db08a9d596b04831c4ccdb9dc2217
Cibernet <41699236+Cibernet83@users.noreply.github.com>
2025-01-04 14:00:56 -0500

    Cracked now damages the item on activate

--- src/main/java/net/cibernet/alchemancy/properties/CrackedProperty.java
+++ src/main/java/net/cibernet/alchemancy/properties/CrackedProperty.java
@@ -1,7 +1,14 @@
 package net.cibernet.alchemancy.properties;
 
+import net.cibernet.alchemancy.item.components.PropertyModifierComponent;
+import net.cibernet.alchemancy.registries.AlchemancyProperties;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.Nullable;
 
 public class CrackedProperty extends Property
 {
@@ -9,6 +16,20 @@ public class CrackedProperty extends Property
 	public int modifyDurabilityConsumed(ItemStack stack, LivingEntity user, int originalAmount, int resultingAmount)
 	{
 		return resultingAmount * (user.level().getRandom().nextFloat() < 0.4f ? 2 : 1);
+	}
+
+	@Override
+	public void onActivation(@Nullable Entity user, Entity target, ItemStack weapon, DamageSource damageSource)
+	{
+
+		if(target.level() instanceof ServerLevel serverLevel && PropertyModifierComponent.getOrElse(weapon, asHolder(), AlchemancyProperties.Modifiers.PREVENT_CONSUMPTION, weapon.isDamageableItem()))
+		{
+			int durabilityConsumed = PropertyModifierComponent.getOrElse(weapon, asHolder(), AlchemancyProperties.Modifiers.DURABILITY_CONSUMPTION, 1);
+			if(user instanceof LivingEntity living)
+				weapon.hurtAndBreak(durabilityConsumed, living, EquipmentSlot.MAINHAND);
+			else weapon.hurtAndBreak(durabilityConsumed, serverLevel, null, (item) -> {});
+		}
+		else consumeItem(user, weapon, EquipmentSlot.MAINHAND);
 	}
 
 	@Override
