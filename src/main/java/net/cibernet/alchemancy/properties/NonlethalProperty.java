0723ac4b213ce84275c0e62e2e4dec366b3303d1
Cibernet <41699236+Cibernet83@users.noreply.github.com>
2025-01-04 14:50:42 -0500

    Nonlethal now triggers On Attack on projectile hit

--- src/main/java/net/cibernet/alchemancy/properties/NonlethalProperty.java
+++ src/main/java/net/cibernet/alchemancy/properties/NonlethalProperty.java
@@ -1,8 +1,13 @@
 package net.cibernet.alchemancy.properties;
 
+import net.cibernet.alchemancy.item.components.InfusedPropertiesHelper;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.HitResult;
+import net.neoforged.neoforge.event.entity.ProjectileImpactEvent;
 import net.neoforged.neoforge.event.entity.living.LivingIncomingDamageEvent;
 
 public class NonlethalProperty extends Property
@@ -10,6 +15,13 @@ public class NonlethalProperty extends Property
 	@Override
 	public void onIncomingAttack(Entity user, ItemStack weapon, LivingEntity target, LivingIncomingDamageEvent event) {
 		event.setCanceled(true);
+	}
+
+	@Override
+	public void onProjectileImpact(ItemStack stack, Projectile projectile, HitResult rayTraceResult, ProjectileImpactEvent event) {
+
+		if(rayTraceResult.getType() == HitResult.Type.ENTITY && rayTraceResult instanceof EntityHitResult entityHitResult && entityHitResult.getEntity() instanceof LivingEntity target)
+			InfusedPropertiesHelper.forEachProperty(stack, propertyHolder -> propertyHolder.value().onAttack(projectile, stack, activationDamageSource(projectile.level(), projectile, projectile.position()), target));
 	}
 
 	@Override
