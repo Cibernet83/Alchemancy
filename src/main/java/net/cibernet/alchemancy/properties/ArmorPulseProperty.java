d44a6b664a176f2191c39d1822832f5d59ed6ee8
Cibernet <41699236+Cibernet83@users.noreply.github.com>
2025-01-10 15:59:22 -0500

    Armor Pulse now activates with self-damage

--- src/main/java/net/cibernet/alchemancy/properties/ArmorPulseProperty.java
+++ src/main/java/net/cibernet/alchemancy/properties/ArmorPulseProperty.java
@@ -1,30 +1,57 @@
 package net.cibernet.alchemancy.properties;
 
 import net.cibernet.alchemancy.item.components.InfusedPropertiesHelper;
+import net.cibernet.alchemancy.properties.data.IDataHolder;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.tags.DamageTypeTags;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
 
-public class ArmorPulseProperty extends Property
+public class ArmorPulseProperty extends Property implements IDataHolder<Boolean>
 {
 	@Override
 	public void modifyDamageReceived(LivingEntity user, ItemStack weapon, EquipmentSlot slot, LivingDamageEvent.Pre event)
 	{
-		if((slot.isArmor() || user.getUseItem() == weapon) && !event.getSource().is(DamageTypeTags.BYPASSES_ARMOR) &&
-				event.getSource().getEntity() != null && event.getSource().getEntity() != user &&
+		if(!getData(weapon) && (slot.isArmor() || user.getUseItem() == weapon) && !event.getSource().is(DamageTypeTags.BYPASSES_ARMOR) &&
+				event.getSource().getEntity() != null &&
+//				event.getSource().getEntity() != user &&
 				event.getSource().getEntity().distanceTo(user) <= user.getAttributeValue(Attributes.ENTITY_INTERACTION_RANGE))
 		{
+			setData(weapon, true);
 			event.setNewDamage(Math.max(0, event.getNewDamage() - 1));
 			InfusedPropertiesHelper.forEachProperty(weapon, propertyHolder -> propertyHolder.value().onActivation(user, user, weapon, event.getSource()));
 		}
 	}
 
+	@Override
+	public void onInventoryTick(Entity user, ItemStack stack, Level level, int inventorySlot, boolean isCurrentItem) {
+
+		if(getData(stack))
+			setData(stack, false);
+	}
 
 	@Override
 	public int getColor(ItemStack stack) {
 		return 0xFF7B60;
+	}
+
+	@Override
+	public Boolean readData(CompoundTag tag) {
+		return tag.getBoolean("activated");
+	}
+
+	@Override
+	public CompoundTag writeData(Boolean data) {
+		return new CompoundTag(){{putBoolean("activated", data);}};
+	}
+
+	@Override
+	public Boolean getDefaultData() {
+		return false;
 	}
 }
