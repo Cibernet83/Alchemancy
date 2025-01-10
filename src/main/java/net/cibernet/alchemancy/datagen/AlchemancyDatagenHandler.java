ff55a50264947e3d797f0f5b4626c35b09607541
Cibernet <41699236+Cibernet83@users.noreply.github.com>
2025-01-10 17:24:36 -0500

    This advancement will give you one million wongas

--- src/main/java/net/cibernet/alchemancy/datagen/AlchemancyDatagenHandler.java
+++ src/main/java/net/cibernet/alchemancy/datagen/AlchemancyDatagenHandler.java
@@ -5,11 +5,9 @@ import net.cibernet.alchemancy.advancements.criterion.DiscoverPropertyTrigger;
 import net.cibernet.alchemancy.item.components.InfusedPropertiesHelper;
 import net.cibernet.alchemancy.properties.Property;
 import net.cibernet.alchemancy.registries.AlchemancyCreativeTabs;
+import net.cibernet.alchemancy.registries.AlchemancyItems;
 import net.cibernet.alchemancy.registries.AlchemancyProperties;
-import net.minecraft.advancements.Advancement;
-import net.minecraft.advancements.AdvancementHolder;
-import net.minecraft.advancements.AdvancementRequirements;
-import net.minecraft.advancements.AdvancementType;
+import net.minecraft.advancements.*;
 import net.minecraft.core.Holder;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
@@ -24,6 +22,7 @@ import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 import net.neoforged.neoforge.registries.DeferredHolder;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
@@ -40,10 +39,11 @@ public class AlchemancyDatagenHandler
 		CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
 
 		generator.addProvider(event.includeServer(), new AdvancementProvider(output, lookupProvider, fileHelper, List.of(
-				AlchemancyDatagenHandler::getPropertyDiscoveryAdvancements
+				AlchemancyDatagenHandler::getPropertyDiscoveryAdvancements,
+				AlchemancyDatagenHandler::getAlchemancyMasterAdvancement
 		)));
 
-		generator.addProvider(event.includeClient(), new PropertyEntryProvider(output, lookupProvider, AlchemancyProperties.WARPED));
+		//generator.addProvider(event.includeClient(), new PropertyEntryProvider(output, lookupProvider, AlchemancyProperties.WARPED));
 	}
 
 	public static void getPropertyDiscoveryAdvancements(HolderLookup.Provider registries, Consumer<AdvancementHolder> saver, ExistingFileHelper existingFileHelper)
@@ -70,6 +70,51 @@ public class AlchemancyDatagenHandler
 
 			builder.save(saver, ResourceLocation.fromNamespaceAndPath(key.getNamespace(), "discovery/" + key.getPath()), existingFileHelper);
 		}
+	}
+
+	private static final List<Holder<Property>> UNOBTAINABLE_PROPERTIES = List.of(
+			AlchemancyProperties.CLAY_MOLD,
+			AlchemancyProperties.SMITING,
+			AlchemancyProperties.AUXILIARY,
+			AlchemancyProperties.RANDOM,
+			AlchemancyProperties.ITEM_MAGNET,
+			AlchemancyProperties.QUANTUM_BIND,
+			AlchemancyProperties.BLOCK_VACUUM,
+			AlchemancyProperties.VOIDTOUCH,
+			AlchemancyProperties.CEASELESS_VOID
+	);
+
+	public static void getAlchemancyMasterAdvancement(HolderLookup.Provider registries, Consumer<AdvancementHolder> saver, ExistingFileHelper existingFileHelper)
+	{
+		Advancement.Builder builder = Advancement.Builder.recipeAdvancement();
+
+		builder.parent(AdvancementSubProvider.createPlaceholder("alchemancy:story/apply_property"));
+
+		builder.display(
+				AlchemancyItems.ALCHEMANCY_CATALYST.asItem(),
+				Component.translatable("advancements.story.alchemancy:all_properties.title"),
+				Component.translatable("advancements.story.alchemancy:all_properties.description"),
+				null,
+				AdvancementType.CHALLENGE,
+				true, true, false
+		);
+
+
+		List<String> requirements = new ArrayList<>();
+		for (DeferredHolder<Property, ? extends Property> property : AlchemancyProperties.REGISTRY.getEntries()) {
+
+			if(UNOBTAINABLE_PROPERTIES.contains(property))
+				continue;
+
+			String key = "discover_" + property.get().getKey().toString();
+			builder.addCriterion(key, DiscoverPropertyTrigger.TriggerInsance.discoverProperty(property));
+			requirements.add(key);
+		}
+		builder.requirements(AdvancementRequirements.allOf(requirements));
+
+		builder.rewards(AdvancementRewards.Builder.experience(1000));
+
+		builder.save(saver, ResourceLocation.fromNamespaceAndPath(Alchemancy.MODID, "story/all_properties"), existingFileHelper);
 	}
 
 }
