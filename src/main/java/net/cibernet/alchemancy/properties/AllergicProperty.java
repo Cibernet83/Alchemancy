2f73b0e1fc4d5f1e413ffd8b0bfbdefe6ae66330
Cibernet <41699236+Cibernet83@users.noreply.github.com>
2025-01-14 12:01:35 -0500

    Fixed Allergic + Tipsy infinite

--- src/main/java/net/cibernet/alchemancy/properties/AllergicProperty.java
+++ src/main/java/net/cibernet/alchemancy/properties/AllergicProperty.java
@@ -2,8 +2,10 @@ package net.cibernet.alchemancy.properties;
 
 import net.cibernet.alchemancy.item.components.InfusedPropertiesHelper;
 import net.cibernet.alchemancy.registries.AlchemancySoundEvents;
+import net.minecraft.core.Holder;
 import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
@@ -13,14 +15,18 @@ import net.neoforged.neoforge.event.entity.living.MobEffectEvent;
 public class AllergicProperty extends AbstractTimerProperty
 {
 
+	private boolean processing = false;
+
 	@Override
 	public void onMobEffectAdded(ItemStack stack, EquipmentSlot slot, LivingEntity user, MobEffectEvent.Added event)
 	{
-		if (isAvailable(stack) && event.getOldEffectInstance() == null)
+		if (!processing && isAvailable(stack) && event.getOldEffectInstance() == null)
 		{
+			processing = true;
+			resetStartTimestamp(stack);
 			user.level().playSound(null, user.getX(), user.getY(), user.getZ(), AlchemancySoundEvents.ALLERGIC.value(), SoundSource.PLAYERS, 0.5f, 1);
 			InfusedPropertiesHelper.forEachProperty(stack, propertyHolder -> propertyHolder.value().onActivation(user, user, stack));
-			resetStartTimestamp(stack);
+			processing = false;
 		}
 	}
 
