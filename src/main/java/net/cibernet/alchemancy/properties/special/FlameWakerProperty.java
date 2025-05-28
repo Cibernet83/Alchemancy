c0ab7ebd2c87936e1948b6ecd9ee3afc8b712231
Cibernet <41699236+Cibernet83@users.noreply.github.com>
2025-05-28 16:22:40 -0400

    Flame Step property real

--- src/main/java/net/cibernet/alchemancy/properties/special/FlameWakerProperty.java
+++ src/main/java/net/cibernet/alchemancy/properties/special/FlameWakerProperty.java
@@ -1,38 +1,44 @@
 package net.cibernet.alchemancy.properties.special;
 
+import net.cibernet.alchemancy.item.components.PropertyModifierComponent;
 import net.cibernet.alchemancy.properties.Property;
+import net.cibernet.alchemancy.registries.AlchemancyProperties;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseFireBlock;
-import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.FireBlock;
 import net.minecraft.world.level.block.state.BlockState;
 
-public class FlameWakerProperty extends Property
-{
+public class FlameWakerProperty extends Property {
 	@Override
-	public void onEquippedTick(LivingEntity user, EquipmentSlot slot, ItemStack stack)
-	{
-		if(slot == EquipmentSlot.FEET || slot == EquipmentSlot.BODY)
+	public void onEquippedTick(LivingEntity user, EquipmentSlot slot, ItemStack stack) {
+		if (user.level().isClientSide() || !(slot == EquipmentSlot.FEET || slot == EquipmentSlot.BODY)) return;
+
+		Level level = user.level();
+		BlockPos pos = user.blockPosition();
+		BlockState state = level.getBlockState(pos);
+
+		if(user.getKnownMovement().length() <= 0.005f && state.is(BlockTags.FIRE) && state.hasProperty(FireBlock.AGE))
 		{
-			Level level = user.level();
-			BlockPos pos = user.blockPosition();
-			if (BaseFireBlock.canBePlacedAt(level, pos, Direction.UP) || level.getBlockState(pos).canBeReplaced())
-			{
-				BlockState blockstate1 = BaseFireBlock.getState(level, pos);
-				level.setBlock(pos, blockstate1, 11);
-			}
+			level.setBlock(pos, state.setValue(FireBlock.AGE, Math.min(state.getValue(FireBlock.AGE), 2)), 11);
+		}
+		if (user.isSprinting() && (BaseFireBlock.canBePlacedAt(level, pos, Direction.UP) || level.getBlockState(pos).canBeReplaced())) {
+			BlockState fireBlock = BaseFireBlock.getState(level, pos);
+			if(fireBlock.hasProperty(FireBlock.AGE))
+				fireBlock = fireBlock.setValue(FireBlock.AGE, 8);
+			level.setBlock(pos, fireBlock, 11);
 		}
 
+		stack.hurtAndBreak(PropertyModifierComponent.getOrElse(stack, asHolder(), AlchemancyProperties.Modifiers.DURABILITY_CONSUMPTION, 1), user, slot);
 	}
 
 	@Override
 	public int getColor(ItemStack stack) {
-		return 0;
+		return 0xFF7022;
 	}
 }
