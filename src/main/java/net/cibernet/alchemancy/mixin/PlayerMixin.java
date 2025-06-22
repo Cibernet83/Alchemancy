60e6ddd65ca34db46468fd0ceb9fc81c861876c9
Cibernet <41699236+Cibernet83@users.noreply.github.com>
2025-06-21 20:38:03 -0400

    Phase Step no longer makes you crouch/crawl

--- src/main/java/net/cibernet/alchemancy/mixin/PlayerMixin.java
+++ src/main/java/net/cibernet/alchemancy/mixin/PlayerMixin.java
@@ -1,11 +1,16 @@
 package net.cibernet.alchemancy.mixin;
 
+import net.cibernet.alchemancy.item.components.InfusedPropertiesHelper;
+import net.cibernet.alchemancy.properties.special.AuxiliaryProperty;
+import net.cibernet.alchemancy.registries.AlchemancyProperties;
 import net.cibernet.alchemancy.util.CommonUtils;
+import net.minecraft.world.entity.Pose;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Player.class)
 public class PlayerMixin
@@ -16,4 +21,9 @@ public class PlayerMixin
 		CommonUtils.tickInventoryItemProperties(((Player)(Object) this));
 	}
 
+	@Inject(method = "canPlayerFitWithinBlocksAndEntitiesWhen", at = @At("HEAD"), cancellable = true)
+	public void canFit(Pose pose, CallbackInfoReturnable<Boolean> cir) {
+		if(InfusedPropertiesHelper.hasItemWithProperty((Player)(Object) this, AlchemancyProperties.PHASE_STEP, true))
+			cir.setReturnValue(true);
+	}
 }
