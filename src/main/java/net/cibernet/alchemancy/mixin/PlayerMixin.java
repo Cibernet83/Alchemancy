ea724b1a61ddfe7102e05430e0856ab511882b08
Cibernet <41699236+Cibernet83@users.noreply.github.com>
2025-08-08 20:43:15 -0400

    Unmovable armor can no longer be swapped out

--- src/main/java/net/cibernet/alchemancy/mixin/PlayerMixin.java
+++ src/main/java/net/cibernet/alchemancy/mixin/PlayerMixin.java
@@ -4,17 +4,22 @@ import net.cibernet.alchemancy.item.components.InfusedPropertiesHelper;
 import net.cibernet.alchemancy.properties.special.AuxiliaryProperty;
 import net.cibernet.alchemancy.registries.AlchemancyProperties;
 import net.cibernet.alchemancy.util.CommonUtils;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.Pose;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Player.class)
-public class PlayerMixin
+public abstract class PlayerMixin
 {
+	@Shadow public abstract ItemStack getItemBySlot(EquipmentSlot slot1);
+
 	@Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;isSpectator()Z", ordinal = 1, shift = At.Shift.BEFORE))
 	public void tickAfterSpectatorCheck(CallbackInfo ci)
 	{
@@ -25,5 +30,11 @@ public class PlayerMixin
 	public void canFit(Pose pose, CallbackInfoReturnable<Boolean> cir) {
 		if(InfusedPropertiesHelper.hasItemWithProperty((Player)(Object) this, AlchemancyProperties.PHASE_STEP, true))
 			cir.setReturnValue(true);
+	}
+
+	@Inject(method = "canUseSlot", at = @At("HEAD"), cancellable = true)
+	public void canUseSlot(EquipmentSlot slot, CallbackInfoReturnable<Boolean> cir) {
+		if(InfusedPropertiesHelper.hasProperty(getItemBySlot(slot), AlchemancyProperties.UNMOVABLE))
+			cir.setReturnValue(false);
 	}
 }
