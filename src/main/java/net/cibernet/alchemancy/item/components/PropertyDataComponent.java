a20cfa5bf89e02f7c52a3f28f76c0627ed7985bb
Cibernet <41699236+Cibernet83@users.noreply.github.com>
2025-08-15 00:28:31 -0400

    Fixed Black Hole tools losing filter when forged

--- src/main/java/net/cibernet/alchemancy/item/components/PropertyDataComponent.java
+++ src/main/java/net/cibernet/alchemancy/item/components/PropertyDataComponent.java
@@ -3,31 +3,45 @@ package net.cibernet.alchemancy.item.components;
 import com.mojang.serialization.Codec;
 import net.cibernet.alchemancy.properties.Property;
 import net.cibernet.alchemancy.properties.data.IDataHolder;
+import net.cibernet.alchemancy.registries.AlchemancyItems;
 import net.cibernet.alchemancy.registries.AlchemancyProperties;
 import net.minecraft.core.Holder;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.CustomData;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-public record PropertyDataComponent(Map<Holder<Property>, CustomData> data)
+public record PropertyDataComponent(Map<Holder<Property>, CompoundTag> data)
 {
 	public static final PropertyDataComponent EMPTY = new PropertyDataComponent(Map.of());
-	public static final Codec<PropertyDataComponent> CODEC = Codec.unboundedMap(Property.CODEC, CustomData.CODEC).xmap(PropertyDataComponent::new, PropertyDataComponent::data);
+	public static final Codec<PropertyDataComponent> CODEC = Codec.unboundedMap(Property.CODEC, CompoundTag.CODEC).xmap(PropertyDataComponent::new, PropertyDataComponent::data);
 	public static final StreamCodec<RegistryFriendlyByteBuf, PropertyDataComponent> STREAM_CODEC = StreamCodec.composite(
-			ByteBufCodecs.map(HashMap::new, Property.STREAM_CODEC, CustomData.STREAM_CODEC),
+			ByteBufCodecs.map(HashMap::new, Property.STREAM_CODEC, ByteBufCodecs.COMPOUND_TAG),
 			PropertyDataComponent::data,
 			PropertyDataComponent::new
 	);
 
 	public Optional<CompoundTag> getDataNbt(Holder<Property> property)
 	{
-		return data.containsKey(property) ? Optional.of(data.get(property).copyTag()) : Optional.empty();
+		return data.containsKey(property) ? Optional.of(data.get(property)) : Optional.empty();
+	}
+
+	public static void mergeData(ItemStack to, ItemStack from)
+	{
+		var toData = to.get(AlchemancyItems.Components.PROPERTY_DATA);
+		var fromData = from.get(AlchemancyItems.Components.PROPERTY_DATA);
+
+		if(fromData == null)
+			return;
+		if(toData == null)
+			to.set(AlchemancyItems.Components.PROPERTY_DATA, fromData);
+		else to.set(AlchemancyItems.Components.PROPERTY_DATA, new Mutable(toData).mergeData(fromData).toImmutable());
 	}
 
 	public boolean isEmpty() {
@@ -43,7 +57,7 @@ public record PropertyDataComponent(Map<Holder<Property>, CustomData> data)
 
 	public static class Mutable
 	{
-		private final HashMap<Holder<Property>, CustomData> data;
+		private final HashMap<Holder<Property>, CompoundTag> data;
 
 		public Mutable(PropertyDataComponent data) {
 			this.data = new HashMap<>(data.data);
@@ -51,12 +65,12 @@ public record PropertyDataComponent(Map<Holder<Property>, CustomData> data)
 
 		public Optional<CompoundTag> getDataNbt(Holder<Property> property)
 		{
-			return data.containsKey(property) ? Optional.of(data.get(property).copyTag()) : Optional.empty();
+			return data.containsKey(property) ? Optional.of(data.get(property)) : Optional.empty();
 		}
 
 		public void setDataNbt(Holder<Property> property, CompoundTag value)
 		{
-			data.put(property, CustomData.of(value));
+			data.put(property, value);
 		}
 
 		public void removeData(Holder<Property> propertyHolder)
@@ -67,6 +81,16 @@ public record PropertyDataComponent(Map<Holder<Property>, CustomData> data)
 		public PropertyDataComponent toImmutable()
 		{
 			return new PropertyDataComponent(data);
+		}
+
+		public Mutable mergeData(PropertyDataComponent other) {
+			for (Map.Entry<Holder<Property>, CompoundTag> entry : other.data().entrySet()) {
+
+				if(data.containsKey(entry.getKey()))
+					data.get(entry.getKey()).merge(entry.getValue());
+				else data.put(entry.getKey(), entry.getValue());
+			}
+			return this;
 		}
 	}
 
